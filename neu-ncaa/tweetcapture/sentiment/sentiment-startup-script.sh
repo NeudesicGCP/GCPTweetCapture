83279ce940f8254a585c8ce6f16be4acf102c579
MikeSherrill <37089605+MikeSherrill@users.noreply.github.com>
2019-02-20 17:30:44 -0800

    Update sentiment-startup-script.sh

--- neu-ncaa/tweetcapture/sentiment/sentiment-startup-script.sh
+++ neu-ncaa/tweetcapture/sentiment/sentiment-startup-script.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/python
-python /$HOME/GCPTweetCapture/neu-ncaa/tweetcapture/sentiment/bigquery-sentiment-analysis-naturallanguage.py ncaa_tweets
+python /HOME/GCPTweetCapture/neu-ncaa/tweetcapture/sentiment/bigquery-sentiment-analysis-naturallanguage.py ncaa_tweets
