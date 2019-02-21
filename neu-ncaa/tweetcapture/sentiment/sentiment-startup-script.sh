a377b278360637c513bb572adcac1855df71a610
MikeSherrill <37089605+MikeSherrill@users.noreply.github.com>
2019-02-21 15:09:27 -0800

    Update sentiment-startup-script.sh

--- neu-ncaa/tweetcapture/sentiment/sentiment-startup-script.sh
+++ neu-ncaa/tweetcapture/sentiment/sentiment-startup-script.sh
@@ -1,5 +1,3 @@
 pip install google-cloud-bigquery
-
 pip install google-cloud-language
-
 python /GCPTweetCapture/neu-ncaa/tweetcapture/sentiment/bigquery-sentiment-analysis-naturallanguage.py ncaa_tweets
