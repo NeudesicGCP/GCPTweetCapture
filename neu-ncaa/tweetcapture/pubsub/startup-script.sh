351366feff48c58e2b0e7f4402e661019dd1a523
MikeSherrill <37089605+MikeSherrill@users.noreply.github.com>
2019-02-20 16:40:28 -0800

    Delete startup-script.sh

--- neu-ncaa/tweetcapture/pubsub/startup-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcloud beta container --project "neu-ncaa" clusters create "ncaatweet-cluster" --zone "us-central1-a" --service-account "neu-ncaa-tweet-serviceaccount@neu-ncaa.iam.gserviceaccount.com"
-cd /$HOME/GCPTweetCapture/neu-ncaa/tweetcapture/pubsub/
-kubectl create -f twitter-stream.yaml
-kubectl create -f bigquery-controller.yaml
