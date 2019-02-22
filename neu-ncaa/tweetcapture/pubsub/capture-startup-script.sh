0945cee68b7f6e0cf0bd603c7df7346b207f4658
MikeSherrill <37089605+MikeSherrill@users.noreply.github.com>
2019-02-22 07:13:52 -0800

    Update capture-startup-script.sh

--- neu-ncaa/tweetcapture/pubsub/capture-startup-script.sh
+++ neu-ncaa/tweetcapture/pubsub/capture-startup-script.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 gcloud beta container --project "neu-ncaa" clusters create "ncaatweet-cluster" --zone "us-central1-a" --service-account "neu-ncaa-tweet-serviceaccount@neu-ncaa.iam.gserviceaccount.com"
 
 gcloud container clusters get-credentials ncaatweet-cluster --zone us-central1-a --project neu-ncaa
