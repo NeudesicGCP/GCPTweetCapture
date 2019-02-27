81c347493b677f7f03950668b4908ce05bb18281
MikeSherrill <37089605+MikeSherrill@users.noreply.github.com>
2019-02-27 09:41:43 -0800

    Update capture-startup-script.sh

--- neu-ncaa/tweetcapture/pubsub/capture-startup-script.sh
+++ neu-ncaa/tweetcapture/pubsub/capture-startup-script.sh
@@ -2,6 +2,10 @@ gcloud beta container --project "neu-ncaa" clusters create "ncaatweet-cluster" -
 
 kubectl config view
 
+export KUBECONFIG=GCPTweetCapture/config
+
+kubectl config view
+
 gcloud container clusters get-credentials ncaatweet-cluster --zone us-central1-a --project neu-ncaa
 
 kubectl create -f GCPTweetCapture/neu-ncaa/tweetcapture/pubsub/twitter-stream.yaml
